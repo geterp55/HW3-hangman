3311e7ec02f44b3a4bc136c2eb5cb978cd73b363
Peter Guerrero <peterguerrero@Peters-MacBook-Pro.local>
2016-05-12 17:38:12 -0500

    hw3 2nd upload

---
+++ assets/javascript/game.js
@@ -0,0 +1,65 @@
+alert("I am connected");
+
+var computerChoices = ['a', 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+
+
+var wordBank = ['tiger','lion','wolf','lynx','owl'];
+var wordGuess = [];
+var winCounter = 0;
+var lossCounter = 0;
+
+// order: set Global Variables
+	//set functions
+	//call functions
+
+//select a random word from wordBank
+
+
+
+// When the user presses the key it records the keypress and then sets it to userguess
+//document.onkeyup = function(event) {
+	//var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+	
+	// This sets the computer guess equal to the random.
+	//var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+	// User chooses d, o or g
+	if ((userGuess == 'd') || (userGuess == 'o') || (userGuess == 'g')){
+		document.log(userGuess);{
+	// if(var j =0; j < )		
+	//}else if (userGuess == 'a'){
+		console.log(computerChoices =='a');
+	}else if (userGuess == 'b'){
+		console.log(computerChoices == 'b')
+	}else if (userGuess == 'c'){
+		console.log(computerChoices == 'c')
+	}else if (userGuess == 'e'){
+		console.log(computerChoices == 'e')
+	}else if (userGuess == 'f'){
+		console.log(computerChoices == 'f')
+	}else if (userGuess == 'g'){
+		console.log(computerChoices == 'g')	
+	}else if (userGuess == 'h'){
+		console.log(computerChoices == 'h')	
+	}else if (userGuess == 'i'){
+		console.log(computerChoices == 'i')
+	}else if (userGuess == 'j'){
+		console.log(computerChoices == 'j')
+	}else if (userGuess == 'k'){
+		console.log(computerChoices == 'k')
+	}else if (userGuess == 'l'){
+		console.log(computerChoices == 'l')
+	}else if (userGuess == 'm'){
+		console.log(computerChoices == 'm')
+	}else if (userGuess == 'n'){
+		console.log(computerChoices == 'n')
+	}else if (userGuess == 'o'){
+		console.log(computerChoices == 'o')
+
+		
+
+	}
+
+}
